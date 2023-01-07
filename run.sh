432cf810a221cb3dd2b6a643fa97f863936f9e02
Willis <williskneeland1234@gmail.com>
2023-01-06 22:34:59 -0700

    copy local contents to dest dir on install

--- run.sh
+++ run.sh
@@ -2,18 +2,19 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-# clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-14-docker-compose $DESTINATION
+
+cp -a ./. ${DESTINATION}/
 rm -rf $DESTINATION/.git
-# set permission
+
 mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
+# sudo chmod -R 777 $DESTINATION
+
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10014/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20014/'$CHAT'/g' $DESTINATION/docker-compose.yml
-# run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml up -d
 
+sed -i 's/9000/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20014/'$CHAT'/g' $DESTINATION/docker-compose.yml
+
+docker-compose -f $DESTINATION/docker-compose.yml up -d
 echo 'Started Odoo @ http://localhost:'$PORT' | Live chat port: '$CHAT
